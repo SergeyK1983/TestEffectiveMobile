dd584e363026d68fd28a00ea3cf50476efbcb1d3
SergeyK1983 <ssp-serg@yandex.ru>
2025-12-23 10:47:48 +0300

    added struct project

---
+++ src/auth_app/repositories/__init__.py
@@ -0,0 +1,5 @@
+from src.auth_app.repositories.user_pwd_repository import UserPasswordRepo
+from src.auth_app.repositories.user_repository import UserRegisterRepo, UserRegisteredRepo
+
+
+__all__ = ["UserRegisterRepo", "UserRegisteredRepo", "UserPasswordRepo"]
